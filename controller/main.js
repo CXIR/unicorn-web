2e7d5921aa0c0c6c6ca8c71fa5775d91de9e05c0
L3Rnumenor <l3r.numenor@icloud.com>
2017-05-04 18:55:53 +0200

    04May17
    Changes on :
    - user profil
    - ride boxes
    - login page and controller ready

--- controller/main.js
+++ controller/main.js
@@ -269,11 +269,40 @@ shareAppControllers.controller('loginCtrl',['$scope',
     function($scope){
         $scope.reset = function(){
             $scope.log = {};
-            angular.element(document.querySelector('#id, #pw')).removeClass('has-error');
+            $scope.error = {};
+            angular.element(document.querySelector('#pin')).removeClass('has-error');
+            angular.element(document.querySelector('#pw')).removeClass('has-error');
         }
 
         $scope.login = function(log){
-            angular.element(document.querySelector('#id')).addClass('has-error');
+          angular.element(document.querySelector('#pin')).removeClass('has-error');
+          angular.element(document.querySelector('#pw')).removeClass('has-error');
+
+          if((log.pin == null && log.pw == null) || (log.pin == '' && log.pw == '')){
+            angular.element(document.querySelector('#pin, #pw')).addClass('has-error');
+            $scope.error = {uncorrect:true,message:'Remplissez les champs ci-dessous'};
+          }
+          else if(log.pin.match(/\s+/g) && log.pw.match(/\s+/g)){
+            angular.element(document.querySelector('#pin, #pw')).addClass('has-error');
+            $scope.error = {uncorrect:true,message:'Remplissez les champs ci-dessous'};
+          }
+          else if(log.pin.match(/\s+/g) || log.pin == null || log.pin == ''){
+            angular.element(document.querySelector('#pin')).addClass('has-error');
+            $scope.error = {uncorrect:true,message:'Votre identifiant est incorrect'};
+          }
+          else if(log.pw.match(/\s+/g) || log.pw == null || log.pw == ''){
+            angular.element(document.querySelector('#pw')).addClass('has-error');
+            $scope.error = {uncorrect:true,message:'Le Mot de Passe est obligatoire'};
+          }
+          else{
+            $scope.loading = true;
+            /*
+            $http.post('',{'pin':log.pin,'pw':log.pw})
+            .then(function(res){
+
+            }, function(res) { console.log('FAIL : '+res.data); });
+            */
+          }
         }
     }
 ]);
